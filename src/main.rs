0c057d38e03152220859d1c0805e8d211b21e08f
manojseetaram <manojseetaram.artytech@gmail.com>
2025-06-06 22:24:24 +0530

    Initial commit: minigrep project

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::env;
+use std::fs;
+use std::process;
+
+use minigrep::{Config, run}; 
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing argument: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for: {}", config.query);
+    println!("In file: {}", config.file_path);
+
+    let contents = fs::read_to_string(&config.file_path)
+        .expect("should have been a file");
+
+    println!("This is a poem.txt file:\n{}", contents);
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
