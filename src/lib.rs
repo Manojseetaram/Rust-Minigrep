98eed55dd6b1e22249b141e5df60fee8c89efaef
manojseetaram <manojseetaram.artytech@gmail.com>
2025-06-07 20:52:18 +0530

    Addding iterstors

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,20 @@
 
-use std::{env, error::Error, fs};
+use std::{env::{self}, error::Error, fs};
 
 
 pub fn search<'a>(query : &str , conents : &'a str,)-> Vec<&'a str>{
-    let mut results = Vec::new();
-   for line in conents.lines(){
-   if line.contains(query){
-    results.push(line)
-   }
-   }
-   results
+//     let mut results = Vec::new();
+//    for line in conents.lines(){
+//    if line.contains(query){
+//     results.push(line)
+//    }
+//    }
+//    results
+
+conents
+    .lines()
+    .filter(|line|line.contains(query))
+    .collect()
 }
 pub fn search_case_insensitive<'a>(
     query: &str,
@@ -32,17 +37,24 @@ pub struct  Config{
     pub file_path : String,
     pub ignore_case : bool
 }
-impl Config {
-    pub fn build(args : &[String])-> Result<Config, &'static str>{
-          if args.len() < 3 {
-            return  Err("not enought arguments");
-          }
-          let query = args[1].clone();
-          let file_path = args[2].clone();
 
-          let ignore_case = env::var("IGNORE_CASE").is_ok();
-          Ok(Config { query, file_path, ignore_case })
-    }
+impl  Config {
+      pub fn build(  mut args: impl Iterator<Item = String>,
+    )-> Result<Config , &'static str>{
+     args.next();
+
+     let query = match args.next() {
+         Some(arg) => arg,
+         None => return Err("Did not get a query string")
+     };
+     let file_path = match  args.next() {
+         Some(arg) => arg,
+         None=> return  Err("Did not  get a file path")
+     };
+            
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        Ok(Config { query, file_path, ignore_case })
+      }
 }
 pub fn run(config : Config)-> Result<() , Box<dyn  Error>>{
       let contents = fs::read_to_string(config.file_path)?;
